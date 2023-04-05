71363ff90fabc0ee6a3805f10b42cb0ffedecc0c
Cody ZUSCHLAG <zuschlac@tetras.univ-savoie.fr>
2023-04-05 09:34:38 +0200

    update home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <div className="text-5xl bg-gradient-to-br from-green-300 via-green-500 to-green-900 bg-clip-text text-transparent font-bold">Welcome home</div>
+        <div className="text-5xl bg-gradient-to-br from-green-300 via-green-500 to-green-900 bg-clip-text text-transparent font-bold">Welcome home CPINFO-22</div>
     )
 }
